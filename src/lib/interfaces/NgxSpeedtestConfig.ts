5b25188028299e87e76e2a25fcba96bd45483149
Supphawit Sripungwiwat <s.sripungwiwat@clbs.co.th>
2022-06-16 14:28:16 +0700

    Remove properties

--- src/lib/interfaces/NgxSpeedtestConfig.ts
+++ src/lib/interfaces/NgxSpeedtestConfig.ts
@@ -2,16 +2,11 @@ export interface NgxSpeedtestConfig {
   domainName: string;
   apiKey: string;
   config?: {
-    latencyTestEnabled: boolean,
-    maxTestPass: number,
-    progress: { enabled: boolean, verbose: boolean },
-    sustainTime: number,
-    testServerEnabled: boolean,
-    uploadTestEnabled: boolean,
-    userInfoEnabled: boolean
-  },
-  showBtn?: boolean;
-  showUI?: boolean;
-  autoStart?: boolean;
-  available?: number
+    sustainTime?: number,
+    testServerEnabled?: boolean,
+    userInfoEnabled?: boolean
+    latencyTestEnabled?: boolean,
+    uploadTestEnabled?: boolean,
+    progress?: { enabled?: boolean, verbose?: boolean },
+  };
 }
