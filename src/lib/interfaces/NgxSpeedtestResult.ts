a027e5ce519987fafbf337b8ef1f274816c8babf
Supphawit Sripungwiwat <s.sripungwiwat@clbs.co.th>
2022-06-17 13:19:27 +0700

    Edit test result interface

--- src/lib/interfaces/NgxSpeedtestResult.ts
+++ src/lib/interfaces/NgxSpeedtestResult.ts
@@ -1,13 +1,13 @@
 export interface NgxSpeedtestResult {
-  hostname: string;
-  ip_address: string;
-  latency: number;
+  hostname?: string;
+  ip_address?: string;
   download: number;
+  latency?: number;
   maxDownload: number;
-  upload: number;
-  maxUpload: number;
-  jitter: number;
-  testDate: string;
-  testServer: string;
-  userAgent: string;
+  upload?: number;
+  maxUpload?: number;
+  jitter?: number;
+  testDate?: string;
+  testServer?: string;
+  userAgent?: string;
 }
